773d5ef7e9bc355b8fe12fe022d15f38f1551a7f
FacuTombesi <facundomtombesi@gmail.com>
2023-01-26 11:49:04 -0300

    navbar + css finished

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,28 +4,43 @@ import styled from "styled-components";
 
 const NavBar = () => {
     return (
-        <div className="navbarContainer">
-            <div className="navbar">
-                <div className="navbarDiv">
-                    <Link className="navbar_link" to="/home">
-                        Home
-                    </Link>
-                </div>
-                <div className="navbarDiv">
-                    <Link className="navbar_link" to="/recipes/create">
-                        Create your recipe
-                    </Link>
-                </div>
-                <div className="navbarDiv">
-                    <Link className="navbar_link" to="/about">
-                        About
-                    </Link>
-                </div>
-            </div>
+        <div>
+            <Nav>
+                <StyledLink to="/home">
+                    Home
+                </StyledLink>
+                <span> • </span>
+                <StyledLink to="/recipes/create">
+                    Create your recipe
+                </StyledLink>
+                <span> • </span>
+                <StyledLink to="/about">
+                    About
+                </StyledLink>
+            </Nav>
         </div>
     )
 };
 
 /* --------------------------------------------------- STYLES --------------------------------------------------- */
+
+const Nav = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 5%;
+    margin-bottom: 5%;
+`
+
+const StyledLink = styled(Link)`
+    padding-inline: 5px 5px;
+    color: #F18817;
+    text-decoration: none;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 110%;
+    font-weight: 500;
+    &:hover {
+        color: black;
+    }
+`
 
 export default NavBar;
