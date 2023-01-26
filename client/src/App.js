1a1878cfa47e176cab2ea9b57ff9b7ff22ad5f1e
FacuTombesi <facundomtombesi@gmail.com>
2023-01-25 21:34:51 -0300

    index and app fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import LandingPage from "./components/LandingPage";
@@ -8,15 +9,15 @@ import About from "./components/About";
 
 function App() {
   return (
-    <>
+    <div>
       <Switch>
         <Route exact path="/" component={LandingPage}></Route>
-        <Route path="/home" component={Home}></Route>
+        <Route exact path="/home" component={Home}></Route>
         <Route path="/recipes/create" component={CreateRecipe}></Route>
         <Route path="/recipes/:id" component={RecipeDetail}></Route>
         <Route path="/about" component={About}></Route>
       </Switch>
-    </>
+    </div>
   )
 };
 
