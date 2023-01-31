1ee9e566f51bd2a371c10f4e147e7c1adf8b6010
FacuTombesi <facundomtombesi@gmail.com>
2023-01-31 16:56:07 -0300

    getRecipeById fix

--- client/src/components/RecipeDetail.jsx
+++ client/src/components/RecipeDetail.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 import { Link, useParams } from "react-router-dom";
+// import { useEffect } from "react";
 import { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import getRecipeById from "../redux/actions"; 
